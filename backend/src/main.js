10d7a2f008548e10c751b80897aa312b57207681
aLilwall <murigorny@gmail.com>
2026-08-10 10:22:34 -0300

    Add new routes and main.js

---
+++ backend/src/main.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const clienteRoutes = require("./routes/clienteRoutes");
+const conexaoMysql = require("./configs/mysql");
+
+const app = express();
+const PORTA = process.env.PORTA || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+app.get("/", function (req, res) {
+    res.send("API ReservLab funcionando")
+});
+
+app.use("/clientes", clientesRoutes);
+
+app.get("/teste-mysql", async function (req, res) {
+    try {
+        const [resultado] = await conexaoMysql.query("SELECT 1 AS teste");
+
+        res.json({
+            mensagem: "Conexão com MySQL bem sucedida",
+            dados: resultado
+        });
+    } catch (erro) {
+        res.status(500).json({
+            mensagem: "Conexão inviável",
+            erro: erro.message
+        });
+    }
+});
+
+app.listen(PORTA, function () {
+    console.log(`Servidor rodando na porta ${PORTA}`)
+});
